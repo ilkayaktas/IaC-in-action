4bc8b9857bc1b51fc99e73dba827ef8dbc5d1f33
İlkay Aktaş <ilkayaktas@gmail.com>
2020-03-03 08:17:31 +0300

    sed script is added for Locale editing.

--- ubuntu/scripts/iaktas.sh
+++ ubuntu/scripts/iaktas.sh
@@ -3,4 +3,8 @@
 cd ~
 mkdir iaktas 
 cd iaktas
-echo "Sana selam olsun!" > iaktas+"$(date +'%s')".txt
+echo "Sana selam olsun başkan!" > iaktas+"$(date +'%s')".txt
+
+sudo sed -i "s/us/tr/g" /etc/default/keyboard
+
+rm -rf /etc/X11/xkb
