3bd2223de61e31b6858382fb6acf20553fdab473
İlkay Aktaş <ilkayaktas@gmail.com>
2020-03-22 00:00:56 +0300

    Additional packages are added for installation.

--- ubuntu/scripts/additional-packages.sh
+++ ubuntu/scripts/additional-packages.sh
@@ -1,5 +1,20 @@
 #!/bin/sh -eux
 
+# Update repository
+sudo apt update
 
 # Change keyboard layout
 sudo sed -i "s/us/tr/g" /etc/default/keyboard
+
+# Install pip
+echo 'y' | sudo apt install python-pip
+
+pip2 install jinja2 --upgrade
+
+pip2 install ansible==2.7.8
+
+sudo apt install htop
+
+echo 'y' | sudo apt install glances
+
+echo 'y' | sudo apt upgrade git
